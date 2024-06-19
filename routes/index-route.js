22a1ea03bca3686abfb36e8820065e50bd71dec7
Sarah Wael <sarahwaelelemam@gmail.com>
2024-06-20 00:50:35 +0300

    login and signup buttons are hidden if user is logged in

--- routes/index-route.js
+++ routes/index-route.js
@@ -1,11 +1,10 @@
+// index-route.js
 const express = require('express');
 const router = express.Router();
 
-// Example of setting loggedIn based on session or authentication status
 router.get('/', (req, res) => {
-    // Example logic to determine if user is logged in
     const loggedIn = req.session.user ? true : false;
-    res.render('index', { loggedIn }); // Pass loggedIn to the view
+    res.render('index', { loggedIn });
 });
 
 module.exports = router;
